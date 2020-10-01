d3ecdf3a10264d4882729fd9c28573a926ac52e6
Guilherme Bordallo <bordallo.guilherme@hotmail.com>
2020-10-01 08:16:14 -0300

    add kafka consumer project in dotnet

---
+++ KafkaConsumer/dotnetKafkaConsumer/dotnetKafkaConsumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace dotnetKafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using var c = new ConsumerBuilder<Ignore, string>(conf).Build();
+            c.Subscribe("quickstart-events");
+
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    var cr = c.Consume(cts.Token);
+                    Console.WriteLine($"Consumed message:\n'{cr.Value}'\nfrom topic {cr.Topic}, partition {cr.Partition}, offset {cr.Offset}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            finally
+            {
+                c.Close();
+            }
+        }
+    }
+}
